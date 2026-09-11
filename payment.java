78453a4e7f0b628b17bca053c4f9a8b6fae9ee7d
Satheesh-ec <satheeshnk99@gmail.com>
2026-09-11 14:47:10 +0000

    Add payment feature

--- payment.java
+++ payment.java
@@ -2,5 +2,12 @@ public class Payment {
 
     public static void main(String[] args) {
         System.out.println("Welcome to my application");
+
+        payment();
+    }
+
+    public static void payment() {
+        System.out.println("Payment successful");
     }
 }
+
