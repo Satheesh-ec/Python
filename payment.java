9228a67478cad377c3699c46250d8f86bffc8a70
Satheesh-ec <satheeshnk99@gmail.com>
2026-09-11 15:06:47 +0000

    Add login feature

--- payment.java
+++ payment.java
@@ -2,5 +2,12 @@ public class Payment {
 
     public static void main(String[] args) {
         System.out.println("Welcome to my application");
+
+        login();
+    }
+
+    public static void login() {
+        System.out.println("User logged in successfully");
     }
 }
+
